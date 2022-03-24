5a46615758a0e31c8081ffffd2c2ade3ad10728d
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-03-24 07:31:47 +0100

    amelioration du code et ajout des fonctionnalités

--- app.js
+++ app.js
@@ -8,32 +8,46 @@ let callingTime = document.querySelector(".calling-timing");
 let callingBtn = document.querySelector(".btn");
 let telephone = document.querySelector(".telephone");
 let tarif = document.querySelector(".tarif");
-
 const activationType = document.getElementById("envoyer");
 const exit = document.getElementById("exit");
 const send = document.getElementById("send");
+const sonnerie = new Audio("./sonne_tel_attente.mp3");
+//const touchSon = new Audio("./numphone-SF.mp3") sonnerie des touches du clavier
 // LOGICS
 for (i =0; i < touch.length; i++){
   touch[i].addEventListener(("click"), (e)=>{
     screen.textContent += e.target.id
-    if (e.target.id == 2){
-      console('2')
-    }
+    //touchSon.play()
   })
 }
 
-let screenChecking = ()=>{
+const timeoutRing = ()=>{
+  setTimeout(() => {
+    sonnerie.play()
+  }, 2000);
+}
+
+const screenChecking = ()=>{
   btnCall.addEventListener(("click"), (e)=>{
     if (screen.textContent == "") {
       telephone.style.position = "absolue";
+      sonnerie.src = "";
     }else if (screen.textContent !== "*425#"){
       calling.style.position = "absolute"
       user.textContent = screen.textContent
     }
     if(screen.textContent == "*425#") {
       tarif.style.top = 25 + "px";
-      activationType.focus()
+      sonnerie.src = "";
+      send.addEventListener(("click"), (e)=>{
+        e.stopPropagation()
+        if(activationType.value === 1) {
+          console.log("le choix du forfait est de 1")
+        }
+      })
+
     }
+    timeoutRing()
   })
 
  exit.addEventListener(("click"), ()=>{
@@ -43,12 +57,13 @@ let screenChecking = ()=>{
  })
   callingBtn.addEventListener(("click"), ()=>{
     callingTime.textContent = "Appel terminé";
+    sonnerie.src = "";
     setTimeout(() => {
       calling.classList.add("calling-remove");    
-      screen.textContent == "";
+      screen.textContent == "";     
     }, 1000);
   })
 
   
 }
-screenChecking()
+screenChecking();
